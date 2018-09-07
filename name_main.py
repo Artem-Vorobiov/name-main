3c6b55744a47696f4b266054111e6000eea1898c
Ärtem-Vorobiovzquitquit()'zz <Artem-Vorobiov>
2018-09-07 20:31:20 +0300

    done

---
+++ name_main.py
@@ -0,0 +1,6 @@
+# We useing this statment only when wnat run functions MAIN () from original first file
+
+if __name__ == '__main__':
+	print('Run Directly')
+else:
+	print('Run From Import')
---
+++ second.py
@@ -0,0 +1,3 @@
+import name_main
+
+print('Second Module Name: {}'.format(__name__))
